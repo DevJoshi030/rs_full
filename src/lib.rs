a408e767bef2694613548e11545128ac7cd7b775
Dev Joshi <devjoshi030@gmail.com>
2023-05-31 22:41:49 +0530

    State Design

--- src/lib.rs
+++ src/lib.rs
@@ -10,11 +10,13 @@ mod object_oriented;
 mod shared_state;
 mod smart_pointers;
 mod smart_pointers_2;
+mod state_design;
 mod threads;
 mod trait_objects;
 mod traits;
 
 pub fn run() {
+    state_design::run();
     trait_objects::run();
     object_oriented::run();
     shared_state::run();
